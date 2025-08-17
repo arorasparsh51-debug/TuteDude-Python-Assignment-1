49c72fe38e73cd6f1a79728077c6a32ba2a0d8c4
arorasparsh51-debug <arorasparsh51@gmail.com>
2025-08-18 00:41:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print("TASK 1")
+a=input('Enter your first number: ')
+b=input('Enter your second number: ')
+
+a=float(a)
+b=float(b)
+
+print('Addition: ',a+b )
+print('Subtraction: ', a-b)
+print('Multiplication: ', a*b )
+print('division: ', a /b )
---
+++ task2.py
@@ -0,0 +1,8 @@
+print('TASK 2')
+a=input('Enter your first Name: ')
+b=input('Enter your last Name:  ')
+
+a=str(a)
+b=str(b)
+
+print('Hello', a+ b,'! welcome to python program')
